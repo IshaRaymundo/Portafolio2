eeac560c3d45f30cd49ce47d9f9763561f83f442
IshaRaymundo <misha.raym@gmail.com>
2023-09-24 18:11:20 -0500

    Se agregó el dash

---
+++ Portafolio_1/portafolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CondicionalAtmosferica from './Pages-Dash/CondicionalAtmos';
+
+function App() {
+  return (
+    
+    <div>
+      <CondicionalAtmosferica/>
+    </div>
+    
+  );
+}
+
+export default App;
